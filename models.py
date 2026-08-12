2ed70b765897cb14cb92eb602e20225a69b223e7
AbbieJonnes <abigaelmwangi534@gmail.com>
2026-08-12 10:57:17 +0300

    fix:update models.py by adding the function songs and the songs details

---
+++ models.py
@@ -0,0 +1,18 @@
+class Song:
+    def __init__(
+        self,
+        title,
+        artist,
+        album="Unknown",
+        artwork="",
+        preview_url="",
+        track_id=None,
+        favorite=False
+    ):
+        self.title = title
+        self.artist = artist
+        self.album = album
+        self.artwork = artwork
+        self.preview_url = preview_url
+        self.track_id = track_id
+        self.favorite = favorite   
